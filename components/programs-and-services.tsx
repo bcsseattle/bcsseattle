89139f4fb33a545edbc74177d848cedc4b9fe059
Muatasim Qazi <muatasimqazi@gmail.com>
2024-06-14 14:41:30 -0500

    update style

--- components/programs-and-services.tsx
+++ components/programs-and-services.tsx
@@ -8,12 +8,12 @@ import Link from 'next/link';
 
 export default function ProgramsAndServices() {
   return (
-    <section className="container mx-auto">
+    <section className="mx-auto">
       <div className="">
         <h2 className="text-2xl font-extrabold text-center py-8">
           Our Programs & Services
         </h2>
-        <div className="container mx-auto">
+        <div className="">
           <div className="flex flex-wrap lg:space-x-12">
             <div className="w-full md:w-1/2 lg:my-4 lg:w-1/3 mb-4">
               <Link href="/funeral-burial">
