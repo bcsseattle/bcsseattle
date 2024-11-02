86b230b71546f23d1c6423fd8af46284bb54e6dd
Muatasim Qazi <muatasimqazi@gmail.com>
2024-11-02 12:03:55 -0700

    add contact us on home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Mission from '@/components/mission';
 import Hero from '@/components/Hero';
 import ProgramsAndServices from '@/components/programs-and-services';
+import ContactForm from '@/components/ui/ctonact-form';
 // import ThreeRows from '@/components/three-rows';
 // import HalfHalf from '@/components/half-half';
 
@@ -11,6 +12,7 @@ export default async function PricingPage() {
       <div className="border-b" />
       <Mission />
       <ProgramsAndServices />
+      <ContactForm />
       {/* <ThreeRows /> */}
       {/* <HalfHalf /> */}
     </>
